a801b40df5ca0171d58b93e7d601719ad074894d
Rezai <mathisrezai2k@gmail.com>
2020-09-13 18:37:37 +0200

    Connexion + Inscription

--- app/src/main/java/com/example/nalone/MainActivity.java
+++ app/src/main/java/com/example/nalone/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.nalone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textViewSinscrire;
     @Override
+    //t'es nul boloss
+    // pire méthode
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewSinscrire = (TextView) findViewById(R.id.textView3);
+
+        textViewSinscrire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(getBaseContext(), SignUpActivity.class);
+                startActivityForResult(signIn, 0);
+            }
+        });
     }
 }
