dda3980a461888be459ddbf9bb01c03249843195
Thibault <thibault.boulay1@gmail.com>
2020-09-21 21:46:24 +0200

    V1

--- app/src/main/java/com/example/nalone/SignUpActivityStudy.java
+++ app/src/main/java/com/example/nalone/SignUpActivityStudy.java
@@ -9,6 +9,6 @@ public class SignUpActivityStudy extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_studyxml);
+        /*setContentView(R.layout.activity_sign_up_studyxml);*/
     }
 }
