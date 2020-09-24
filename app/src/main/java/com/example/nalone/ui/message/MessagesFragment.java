46804b56e0a24f9c2d7c3d1904b841710be7cedd
hugo <hlegal44@hotmail.fr>
2020-09-24 11:34:07 +0200

    oui

--- app/src/main/java/com/example/nalone/ui/message/MessagesFragment.java
+++ app/src/main/java/com/example/nalone/ui/message/MessagesFragment.java
@@ -4,34 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.nalone.R;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MessagesFragment extends Fragment {
 
     private MessagesViewModel messagesViewModel;
+    private TextView affMessage;
+    private Button buttonSend;
+    private EditText messageEditText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("NoLonely");
+
+
+
+
+
+
         messagesViewModel =
                 ViewModelProviders.of(this).get(MessagesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_messages, container, false);
-        final TextView textView = root.findViewById(R.id.text_messages);
+
+        //instance
+        affMessage = root.findViewById(R.id.affMessage);
+        buttonSend = root.findViewById(R.id.buttonSend);
+
+
         messagesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+
+
+
+
             }
         });
         return root;
     }
+
+
+
+
 }
