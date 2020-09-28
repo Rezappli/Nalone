198f38f1da0dbd207bef32230f36643162d7d116
Thibault4 <thibault.boulay1@gmail.com>
2020-09-28 13:52:14 +0200

    V3

--- app/src/main/java/com/example/nalone/ui/recherche/RechercheFragment.java
+++ app/src/main/java/com/example/nalone/ui/recherche/RechercheFragment.java
@@ -32,7 +32,7 @@ public class RechercheFragment extends Fragment {
         rechercheViewModel =
                 ViewModelProviders.of(this).get(RechercheViewModel.class);
         View root = inflater.inflate(R.layout.fragment_recherche, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.text_recherche);
         rechercheViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
