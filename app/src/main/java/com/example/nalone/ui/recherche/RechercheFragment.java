283b561bfa7977fe83dfda7f222775e979ba0e96
Thibault4 <thibault.boulay1@gmail.com>
2020-10-02 19:28:01 +0200

    V4.4

--- app/src/main/java/com/example/nalone/ui/recherche/RechercheFragment.java
+++ app/src/main/java/com/example/nalone/ui/recherche/RechercheFragment.java
@@ -14,11 +14,19 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.nalone.HomeActivity;
 import com.example.nalone.ItemPerson;
 import com.example.nalone.ItemPersonAdapter;
 import com.example.nalone.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RechercheFragment extends Fragment {
 
@@ -37,7 +45,7 @@ public class RechercheFragment extends Fragment {
 
         rechercheViewModel =
                 ViewModelProviders.of(this).get(RechercheViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_recherche, container, false);
+        final View root = inflater.inflate(R.layout.fragment_recherche, container, false);
 
         search_bar = root.findViewById(R.id.search_bar);
 
@@ -77,29 +85,85 @@ public class RechercheFragment extends Fragment {
             }
         });
 
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
-        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
+        DatabaseReference id_users_ref = FirebaseDatabase.getInstance().getReference("id_users");
+        id_users_ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final String id_user = snapshot.getValue(String.class);
+                final int nb_users = Integer.parseInt(id_user);
+                for(int i = 0; i < nb_users; i++){
+                    DatabaseReference user = FirebaseDatabase.getInstance().getReference("users/"+i);
+                    final int finalI = i;
+                    user.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String mail = snapshot.child("mail").getValue(String.class);
+
+                            if(mail.equalsIgnoreCase(HomeActivity.user_mail)){
+                                int id_user_connect = finalI;
+                                String amis_text = snapshot.child("amis").getValue(String.class);
+                                final List<String> liste_amis = Arrays.asList(amis_text.split(","));
+
+                                Log.w("Liste", "Liste amis:"+amis_text);
+                                items.clear();
+                                for(int j = 0; j < nb_users; j++){
+                                    Log.w("Liste", "J :"+j);
+                                    if(!liste_amis.contains(j+"") && j != finalI){
+                                        DatabaseReference user_found = FirebaseDatabase.getInstance().getReference("users/"+j);
+                                        user_found.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                String prenom = snapshot.child("prenom").getValue(String.class);
+                                                String nom = snapshot.child("nom").getValue(String.class);
+                                                Log.w("Liste", "Ajout de :"+prenom+ " " +nom);
+                                                mRecyclerView = root.findViewById(R.id.recyclerView);
+                                                mLayoutManager = new LinearLayoutManager(getContext());
+
+                                                mRecyclerView.setLayoutManager(mLayoutManager);
+                                                mRecyclerView.setAdapter(mAdapter);
+                                                items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, prenom+" "+nom, R.drawable.ic_baseline_add_24));
+                                                /*if(items.size() == liste_amis.size()){
+                                                    dialogAddPerson.show();
+                                                }*/
+
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                            }
+                                        });
+                                    }
+                                }
+
+                                mAdapter = new ItemPersonAdapter(items);
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
-        mRecyclerView = root.findViewById(R.id.recyclerView);
+        /*mRecyclerView = root.findViewById(R.id.recyclerView);
         mAdapter = new ItemPersonAdapter(items);
         mLayoutManager = new LinearLayoutManager(getContext());
 
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(mAdapter);*/
 
         return root;
 
