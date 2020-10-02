badfcca858b7f9294ca3b2bbf814cae04fa44091
Thibault4 <thibault.boulay1@gmail.com>
2020-10-02 18:42:52 +0200

    V4.4

--- app/src/main/java/com/example/nalone/ui/recherche/RechercheFragment.java
+++ app/src/main/java/com/example/nalone/ui/recherche/RechercheFragment.java
@@ -1,86 +1,105 @@
 package com.example.nalone.ui.recherche;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.nalone.ItemPerson;
+import com.example.nalone.ItemPersonAdapter;
 import com.example.nalone.R;
-import com.google.android.gms.common.internal.SimpleClientAdapter;
 
-import org.xmlpull.v1.XmlPullParser;
+import java.util.ArrayList;
 
 public class RechercheFragment extends Fragment {
 
     private RechercheViewModel rechercheViewModel;
-    private LinearLayout layout_fond;
-    private LinearLayout recherche_layout;
+    private SearchView search_bar;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        //((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
-        //((AppCompatActivity) getActivity()).getSupportActionBar().setIcon(R.drawable.logo);
-        //((AppCompatActivity) getActivity()).getSupportActionBar().setIcon(R.drawable.logo);
-        //((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("NoLonely");
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-
+        final ArrayList<ItemPerson> items = new ArrayList<>();
 
         rechercheViewModel =
                 ViewModelProviders.of(this).get(RechercheViewModel.class);
         View root = inflater.inflate(R.layout.fragment_recherche, container, false);
 
-        layout_fond = root.findViewById(R.id.fond_layout);
-        recherche_layout = root.findViewById(R.id.recherche_layout);
+        search_bar = root.findViewById(R.id.search_bar);
 
-        recherche_layout.setVisibility(View.INVISIBLE);
-
-        LinearLayout duplicate_layout = new LinearLayout(getContext());
-        duplicate_layout.setLayoutParams(recherche_layout.getLayoutParams());
-
-        CardView firstCardView = new CardView(getContext());
-        firstCardView.setLayoutParams(recherche_layout.getChildAt(0).getLayoutParams());
-        duplicate_layout.addView(firstCardView);
-
-        LinearLayout linearCardView = new LinearLayout(getContext());
-        linearCardView.setLayoutParams(recherche_layout.getChildAt(1).getLayoutParams());
-        firstCardView.addView(linearCardView);
-
-        CardView secondCardView = new CardView(getContext());
-        secondCardView.setLayoutParams(recherche_layout.getChildAt(2).getLayoutParams());
-        linearCardView.addView(secondCardView);
-
-        ImageView image = new ImageView(getContext());
-        image.setImageResource(R.drawable.logo);
-        secondCardView.addView(image);
-
-//        duplicate_layout.addView(firstCardView);
-
-        layout_fond.addView(duplicate_layout);
-
-
-
-
-
-        rechercheViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        search_bar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-
+            public void onClick(View v) {
+                    search_bar.onActionViewExpanded();
             }
         });
+
+        search_bar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.w("Recherche", "Le texte change :"+newText);
+                final ArrayList<ItemPerson> tempList = new ArrayList<>();
+                if(newText.length() > 0) {
+                    for (int i = 0; i < items.size(); i++) {
+                        Log.w("Recherche", "On re"+newText);
+                        if (items.get(i).getNom().contains(newText)) {
+                            tempList.add(items.get(i));
+                        }
+                    }
+                    Log.w("Recherche", "Refresh de la liste ");
+                    mAdapter = new ItemPersonAdapter(tempList);
+                    mRecyclerView.setAdapter(mAdapter);
+
+                }else{
+                    mAdapter = new ItemPersonAdapter(items);
+                    mRecyclerView.setAdapter(mAdapter);
+                }
+                return false;
+            }
+        });
+
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 1", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 2", R.drawable.ic_baseline_add_24));
+        items.add(new ItemPerson(R.drawable.ic_baseline_account_circle_24, "User 3", R.drawable.ic_baseline_add_24));
+
+
+        mRecyclerView = root.findViewById(R.id.recyclerView);
+        mAdapter = new ItemPersonAdapter(items);
+        mLayoutManager = new LinearLayoutManager(getContext());
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
         return root;
 
