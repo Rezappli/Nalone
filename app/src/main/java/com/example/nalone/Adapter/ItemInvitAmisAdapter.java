662cf2afbb371ef496a386430a0475ef26854a0a
Rezappli <66002646+Rezappli@users.noreply.github.com>
2020-10-18 21:38:52 +0200

    Invit

---
+++ app/src/main/java/com/example/nalone/Adapter/ItemInvitAmisAdapter.java
@@ -0,0 +1,94 @@
+package com.example.nalone.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nalone.ItemPerson;
+import com.example.nalone.R;
+
+import java.util.List;
+
+public class ItemInvitAmisAdapter extends RecyclerView.Adapter<ItemInvitAmisAdapter.ItemInvitViewHolder> {
+    private List<ItemPerson> mItemPersonList;
+    public OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onAddClick(int position);
+        void onRemoveClick(int position);
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener= listener;
+    }
+
+
+    public static class ItemInvitViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mText;
+        public ImageView mImageViewAdd;
+        public ImageView mImageViewRemove;
+
+        public ItemInvitViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageInvitAmis);
+            mText = itemView.findViewById(R.id.nomAmisInvit);
+            mImageViewAdd = itemView.findViewById(R.id.addInvitAmis);
+            mImageViewRemove = itemView.findViewById(R.id.removeInvitAmis);
+
+            mImageViewAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onAddClick(position);
+                        }
+                    }
+                }
+            });
+
+            mImageViewRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onRemoveClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemInvitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_invit_amis, parent, false);
+        ItemInvitViewHolder ipvh = new ItemInvitViewHolder(v, mListener);
+        return ipvh;
+    }
+    public ItemInvitAmisAdapter(List<ItemPerson> itemlist){
+        mItemPersonList = itemlist;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ItemInvitViewHolder holder, int position) {
+        ItemPerson currentItem = mItemPersonList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mText.setText((currentItem.getNom()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemPersonList.size();
+    }
+
+}
--- app/src/main/java/com/example/nalone/ui/amis/display/Fragment_3.java
+++ app/src/main/java/com/example/nalone/ui/amis/display/Fragment_3.java
@@ -2,13 +2,33 @@ package com.example.nalone.ui.amis.display;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.nalone.Adapter.ItemAmisAdapter;
+import com.example.nalone.Adapter.ItemFiltreAdapter;
+import com.example.nalone.Adapter.ItemInvitAmisAdapter;
+import com.example.nalone.Adapter.ItemProfilAdapter;
+import com.example.nalone.ItemPerson;
 import com.example.nalone.R;
+import com.example.nalone.util.Constants;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.nalone.util.Constants.user_mail;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +36,10 @@ import com.example.nalone.R;
  * create an instance of this fragment.
  */
 public class Fragment_3 extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private ItemInvitAmisAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +79,112 @@ public class Fragment_3 extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+
+        final View root = inflater.inflate(R.layout.fragment_3, container, false);
+        final List<ItemPerson> invits = new ArrayList<>();
+
+
+        DatabaseReference id_users_ref = Constants.firebaseDatabase.getReference("id_users");
+        id_users_ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final String id_user = snapshot.getValue(String.class);
+                final int nb_users = Integer.parseInt(id_user);
+                for(int i = 0; i < nb_users; i++){
+                    DatabaseReference user = Constants.firebaseDatabase.getReference("users/"+i);
+                    final int finalI = i;
+                    user.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String mail = snapshot.child("mail").getValue(String.class);
+
+                            if(mail.equalsIgnoreCase(user_mail)){
+                                int id_user_connect = finalI;
+                                String invit_text = snapshot.child("demande_amis_recu").getValue(String.class);
+                                final List<String> liste_amis = Arrays.asList(invit_text.split(","));
+
+                                Log.w("Liste", "Liste amis:"+invit_text);
+                                invits.clear();
+
+                                if(liste_amis.size() == nb_users-1){
+                                    Log.w("Recherche","Print aucun amis a ajouter");
+                                   // resultat.setVisibility(View.VISIBLE);
+                                   // resultat.setText("Aucun amis à ajouter !");
+                                }
+
+                                for(int j = 0; j < nb_users; j++){
+                                    Log.w("Liste", "J :"+j);
+                                    if(liste_amis.contains(j+"") && j != finalI){
+                                        DatabaseReference user_found = Constants.firebaseDatabase.getReference("users/"+j);
+                                        final int finalJ = j;
+                                        user_found.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                String prenom = snapshot.child("prenom").getValue(String.class);
+                                                String nom = snapshot.child("nom").getValue(String.class);
+                                                String desc = snapshot.child("description").getValue( String.class );
+                                                String nbCreate = snapshot.child("nombre_creation").getValue(String.class);
+                                                String nbParticipate = snapshot.child("nombre_participation").getValue(String.class);
+                                                Log.w("Liste", "Ajout de :"+prenom+ " " +nom);
+                                                mRecyclerView = root.findViewById(R.id.recyclerViewInvitAmis);
+                                                mLayoutManager = new LinearLayoutManager(getContext());
+
+                                                mRecyclerView.setLayoutManager(mLayoutManager);
+                                                mRecyclerView.setAdapter(mAdapter);
+                                                invits.add(new ItemPerson(finalJ,R.drawable.ic_baseline_account_circle_24, prenom+" "+nom, 0, desc, nbCreate, nbParticipate));
+                                                /*if(items.size() == liste_amis.size()){
+                                                    dialogAddPerson.show();
+                                                }*/
+                                                mAdapter.setOnItemClickListener(new ItemInvitAmisAdapter.OnItemClickListener() {
+                                                    @Override
+                                                    public void onAddClick(int position) {
+                                                        Log.w("clickInvit", "Add");
+                                                    }
+
+                                                    @Override
+                                                    public void onRemoveClick(int position) {
+                                                        Log.w("clickInvit", "Remove");
+                                                    }
+                                                });
+
+
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                            }
+                                        });
+                                    }
+                                }
+
+                                mAdapter = new ItemInvitAmisAdapter(invits);
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return root;
     }
 }
