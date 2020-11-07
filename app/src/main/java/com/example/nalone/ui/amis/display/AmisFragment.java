34302fe8470c9ac8beec21c2a319edd782972ab7
Thibault4 <thibault.boulay1@gmail.com>
2020-11-07 12:28:12 +0100

    9.5.1 Solve delete friend errors

--- app/src/main/java/com/example/nalone/ui/amis/display/AmisFragment.java
+++ app/src/main/java/com/example/nalone/ui/amis/display/AmisFragment.java
@@ -34,21 +34,7 @@ import static com.example.nalone.util.Constants.heightScreen;
 import static com.example.nalone.util.Constants.listeners;
 import static com.example.nalone.util.Constants.widthScreen;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AmisFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AmisFragment extends Fragment implements CoreListener{
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private SearchView search_bar;
     private RecyclerView mRecyclerView;
@@ -62,33 +48,6 @@ public class AmisFragment extends Fragment implements CoreListener{
 
     public AmisFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AmisFragment newInstance(String param1, String param2) {
-        AmisFragment fragment = new AmisFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
