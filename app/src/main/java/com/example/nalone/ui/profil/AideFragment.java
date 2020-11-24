73fcdad1a168eb3ce32ae9f6b15ae299186c419e
Thibault4 <thibault.boulay1@gmail.com>
2020-11-24 14:33:15 +0100

    15 SURPRISSEEE

--- app/src/main/java/com/example/nalone/ui/profil/AideFragment.java
+++ app/src/main/java/com/example/nalone/ui/profil/AideFragment.java
@@ -4,20 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.nalone.QrCode;
 import com.example.nalone.R;
+
+import static com.example.nalone.util.Constants.USER;
 
 public class AideFragment extends Fragment {
 
-
+    private ImageView qr_code;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_aide, container, false);
+
+        qr_code = root.findViewById(R.id.qr_code_image);
+        qr_code.setImageBitmap(QrCode.generate(USER.getUid()));
 
         return root;
     }
