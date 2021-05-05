4e33c9050462e5f0952608f63043b5c77cc150a8
Thibault Boulay <tboulay@oet.fr>
2021-05-05 14:10:12 +0200

    NotificationService => Service background

--- app/src/main/java/splash/MyApp.java
+++ app/src/main/java/splash/MyApp.java
@@ -1,16 +1,50 @@
 package splash;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
 import android.os.SystemClock;
+import android.util.Log;
 
-import com.example.nalone.util.Constants;
+import com.example.nalone.NotificationService;
+import com.example.nalone.Restarter;
 
 
 public class MyApp extends Application {
 
+    Intent mServiceIntent;
+    private NotificationService mYourService;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        //SystemClock.sleep(2000)
+        mYourService = new NotificationService();
+        mServiceIntent = new Intent(this, mYourService.getClass());
+        if (!isMyServiceRunning(mYourService.getClass())) {
+            startService(mServiceIntent);
+        }
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i ("Service status", "Running");
+                return true;
+            }
+        }
+        Log.i ("Service status", "Not running");
+        return false;
+    }
+
+    @Override
+    public void onTerminate(){
+        //stopService(mServiceIntent);
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction("restartservice");
+        broadcastIntent.setClass(this, Restarter.class);
+        this.sendBroadcast(broadcastIntent);
+        super.onTerminate();
     }
 }
